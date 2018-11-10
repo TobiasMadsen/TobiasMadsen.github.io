19b7200cecb24b9775ddce4f0e14223fea0af4fa
Tobias Madsen <tobias.madsen@scioplus.com>
2018-11-10 10:16:16 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,36 +26,36 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3ad208de4bd38e7606ac.js"
+    "url": "webpack-runtime-9d096977bcecaa91b762.js"
   },
   {
-    "url": "app-397fa35e17df8e0a7328.js"
+    "url": "app-fbccdf92b95465f794e7.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-a5814bd9535675e5b054.js"
   },
   {
     "url": "index.html",
-    "revision": "efcd526b202ba367297a33637f30408a"
+    "revision": "6e2910732dd96de3d82de0a510533a3d"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d88c8751945519be8a13ef88361d9473"
+    "revision": "9029082d1aa84923d6df273c6ff1ccc7"
   },
   {
-    "url": "0.3165c193e52a3ef7383d.css"
+    "url": "0.3318011a1539b67df169.css"
   },
   {
     "url": "1.3d83c616a2f8947ca2fc.css"
   },
   {
-    "url": "1-4c2e0ab5837be2720c51.js"
+    "url": "1-b6606d0f03ea942cc535.js"
   },
   {
-    "url": "component---src-pages-index-js-fee5866d8cba9ab50462.js"
+    "url": "component---src-pages-index-js-6e85a784a4bee168d870.js"
   },
   {
-    "url": "0-f89f30f01d3655fe51ca.js"
+    "url": "0-9eaa5e58a127920e6e01.js"
   },
   {
     "url": "static/d/151/path---index-6a9-hkeSZ2gGRB0ow38jKggzEKncY.json",
